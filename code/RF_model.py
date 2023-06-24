5e99296311f62a85e0b44b6c5b2928375a7aef08
maplumridge <map205@cam.ac.uk>
2023-06-24 15:34:30 +0100

    Calculate min_pixels in script. Copy X_test data frame to use for CSV generation. Remove original_indeces (not needed anymore). Log verbose metrics for grid search cross validation.

--- code/RF_model.py
+++ code/RF_model.py
@@ -1,7 +1,23 @@
 """ 
+To do:
+- Tidy up
+- if input is L1 or L2, save CSV with downstream ground truth (L2/L3)
+- else, save CSV without downstream ground truth, only predictions
+
+Feeds into:
+- model --> practice_test_RF_model.py (working OK)
+- CSV --> practice_H_train_RF_model.py (to be tested)
+
 This finally works!!!! To be cleaned up...
+Running...
 
 Updates since last push:
+- This now outputs the correct CSV, with:
+predictions, satellite data and L2 groundtruth for the same row
+- Calculate min pixels per class in script, rather than requiring as command-line arg.
+- Logging accuracy per hyperparm combination
+
+Pushed:
 - Enforce command-line args
 - Added all class labels
 - Rearrange CSV generation (order was incorrect so file was not being saved)
@@ -70,7 +86,7 @@ parser = argparse.ArgumentParser(description='Land Cover Classification')
 parser.add_argument('--satellite_files', nargs='*', help='List of satellite data files to generate time series stack (4 bands, 10 bands, 12 bands)', required=True)
 parser.add_argument('--groundtruth_file', help='Ground truth data file (level 1 or 2)', required=True)
 parser.add_argument('--additional_groundtruth_file', help='Lower level ground truth file for level 2 or 3 classification', required=True)
-parser.add_argument('--pixels_per_class', type=int, help='Number of pixels per class to use for model training', required=True)
+#parser.add_argument('--pixels_per_class', type=int, help='Number of pixels per class to use for model training', required=True)
 parser.add_argument('--label_type', choices=['L1', 'L2', 'L3'], default='L1', help='Labels to use for confusion matrix (level 1, 2 or 3 classes)', required=True)
 parser.add_argument('--model_output', help='Name of output pkl file which model will be saved to', required=True)
 parser.add_argument('--model_predictions', help='Name of output CSV file which model predictions will be saved to', required=True)
@@ -90,7 +106,7 @@ print(satellite_files)
 satellite_files = [os.path.join(input_dir, file) for file in args.satellite_files]
 # Load CORINE ground truth data with level 1 or 2 classes
 groundtruth_file = os.path.join(input_dir, args.groundtruth_file)
-# Load additional CORINE ground truth data with 2 or 3 classes
+# Load additional CORINE ground truth data with level 2 or 3 classes
 additional_groundtruth_file = os.path.join(input_dir, args.additional_groundtruth_file)
 
 
@@ -118,25 +134,6 @@ rows, cols = groundtruth_data.shape
 groundtruth_data = np.resize(groundtruth_data, (rows, cols))
 groundtruth_data_flat = groundtruth_data.flatten()
 
-### No longer want to remove pixels with certain classes, train model on all so that it can be used in multiple settings
-## Remove classes we are not interested in (ocean) & NaN values
-## Convert ground truth to float
-#ground_truth_data = ground_truth_data.astype(float)
-## Create mask for "Water bodies" class
-#mask = ground_truth_data == 5
-## Apply mask to convert class 5 to NaN
-#ground_truth_data[mask] = np.nan
-
-
-### CREATE DATA FRAME ###
-## Create a DataFrame combining data and ground truth data
-#df = pd.DataFrame(satellite_data_2d, columns=['band'+str(i) for i in range(1, bands+1)])
-#df['groundtruth'] = groundtruth_data_flat
-## Remove rows with NaN values
-#df.dropna(inplace=True)
-##print(df.head(10))
-
-
 ### ADDITIONAL GROUND TRUTH DATA ###
 # Open additional ground truth data with gdal
 L2_groundtruth_image = gdal.Open(additional_groundtruth_file)
@@ -144,19 +141,26 @@ L2_groundtruth_image = gdal.Open(additional_groundtruth_file)
 L2_groundtruth_data = L2_groundtruth_image.GetRasterBand(1).ReadAsArray()
 L2_groundtruth_data_flat = L2_groundtruth_data.flatten()
 
-
 ### CREATE DATA FRAME ###
-# Create a DataFrame combining data, ground truth data, and L2 ground truth data
+# Combine satellite time-series, ground truth data, and L2 ground truth data
 df = pd.DataFrame(satellite_data_2d, columns=['band'+str(i) for i in range(1, bands+1)])
 df['groundtruth'] = groundtruth_data_flat
 df['L2_groundtruth'] = L2_groundtruth_data_flat
 print("Data frame before NaN removal (L1, L2 and satellite data)", df)
-## Remove rows with NaN values
+# Remove rows with NaN values
 df.dropna(inplace=True)
 print("Data frame post-NaN removal (L1, L2 and satellite data):", df)
 
 
 ### ORGANISE DATA FOR MODELLING ###
+# DF sampling: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.sample.html
+# DF axes management: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.reset_index.html
+# DF count: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.value_counts.html
+# DF drop: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
+# DF drop NaN: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.dropna.html
+# DF grouping: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.groupby.html
+# DF apply & lambda : https://sparkbyexamples.com/pandas/pandas-apply-with-lambda-examples/
+
 
 # New addition:
 # First, shuffle the data before train/test split, to randomise selection of data for training and testing
@@ -168,15 +172,24 @@ shuffled_df = df.sample(frac=1, random_state=42)
 # This works and shows that the index values are maintained and the pixels are shuffled
 print("Shuffled data frame:", shuffled_df) 
 
-# I want to use the same number of pixels for each class (oversampling/undersampling), to create a balanced model
-min_pixels_per_class = args.pixels_per_class
+## OLD WORKING CODE: I want to use the same number of pixels for each class (oversampling/undersampling), to create a balanced model
+#min_pixels_per_class = args.pixels_per_class
+
+### Change min_pixels_per_class to something like the following:
+# Count number of pixels per class in 'groundtruth' column of data frame...
+# ... this column corresponds to groundtruth_data_flat
+pixels_per_class = shuffled_df['groundtruth'].value_counts()
+min_pixels_per_class = pixels_per_class.min()
+print("Min pixels per class:", min_pixels_per_class)
 
 ## Old sampling code before we had the shuffle...
 #df_level1_sample = shuffled_df.groupby('groundtruth_level1').apply(lambda x: x.sample(n=min_pixels_per_class, replace=True, random_state=42)).reset_index(drop=True)
 #print("DF with 100 pixels per class:", df_level1_sample.head(10))
 
 # Now, group shuffled data by 'groundtruth' class 
-# & sample specified (equal) number of pixels per class
+# & sample min_pixels_per_class
+# don't want to reset index 
+# see https://stackoverflow.com/questions/51866908/difference-between-as-index-false-and-reset-index-in-pandas-groupby
 df_sample = shuffled_df.groupby('groundtruth', as_index=False).apply(lambda x: x.sample(n=min_pixels_per_class, replace=True, random_state=42)).reset_index(level=0, drop=True)
 
 ## Add original indexes as a column in df_sample <-- do not need this any more...
@@ -207,7 +220,7 @@ print("Shuffled Testing Data:")
 print("Satellite testing data:", X_test)
 print("Ground truth testing data:", y_test)
 
-# Calculate number of pixels per class, to ensure it's roughly equal
+# Calculate number of pixels per class, follwoing train/test split to ensure it's roughly equal
 train_pixels_per_class = y_train.value_counts()
 test_pixels_per_class = y_test.value_counts()
 print("Number of pixels per class (Training Data):")
@@ -215,29 +228,32 @@ print(train_pixels_per_class)
 print("Number of pixels per class (Testing Data):")
 print(test_pixels_per_class)
 
-# Record the indices of the testing/prediction dataset
-original_indices_test = X_test.index
-print("Test indices:", X_test.index)
+## DO NOT NEED THIS ANY MORE
+## Record indices of testing dataset only
+#original_indices_test = X_test.index
+#print("Test indices:", X_test.index)
 
 # Remove the 'L2_groundtruth' column from X_train, maintain for X_test
+# Ready for modelling
 X_train.drop('L2_groundtruth', axis=1, inplace=True)
-# Create a copy of X_test (with L2_groundtruth, for use in CSV file)
+# Create a copy of X_test (with 'L2_groundtruth', for use in CSV file)
 X_test_CSV = X_test.copy()
+# Ready for modelling
 X_test.drop('L2_groundtruth', axis=1, inplace=True)
 
 
 ## MODELING ##
-## Specify model configuration
 # First, define the parameter grid for 5-fold grid-search cross-validation
 # These are the specific parameter values that I want to test
 
+## grid search vs random search : https://www.kdnuggets.com/2022/10/hyperparameter-tuning-grid-search-random-search-python.html
 param_grid = {
     'n_estimators': [100, 200, 300, 500],
     'max_depth': [5, 10, 20, 30],
     'min_samples_leaf': [1, 2, 3, 5]
 }
 """
-For practicing...
+Fewer params for practicing...
 
 param_grid = {
     'n_estimators': [100, 200],
@@ -250,7 +266,9 @@ param_grid = {
 rf = RandomForestClassifier(random_state=42, n_jobs=-1)
 # Train RF model (note that I am using 5 folds)
 # Maybe also modify this to be a command-line argument...?
-grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5)
+# Adding verbose mode https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+# https://stackoverflow.com/questions/24121018/sklearn-gridsearch-how-to-print-out-progress-during-the-execution
+grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=5, verbose=2)
 grid_search.fit(X_train, y_train)
 # Find best model and its hyperparameters
 best_model = grid_search.best_estimator_
@@ -266,6 +284,24 @@ y_pred = best_model.predict(X_test)
 # Overall accuracy
 accuracy = accuracy_score(y_test, y_pred)
 print("Test Accuracy:", accuracy)
+
+## NEW TEST ##
+# Metrics from grid search CV
+# See https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html
+print("SKlearn metrics:")
+# Print all hyperparameter combinations and their results
+results = grid_search.cv_results_
+for i in range(len(results['params'])):
+    print("Hyperparameters:", results['params'][i])
+    print("Mean test score:", results['mean_test_score'][i])
+    print("Standard deviation test score:", results['std_test_score'][i])
+    print()
+
+# Log best parameters
+print()
+print("Best parameters:", best_params)
+wandb.config.update(best_params)
+### END OF NEW CODE ###
 
 # Precision
 # See https://scikit-learn.org/stable/modules/generated/sklearn.metrics.precision_score.html#sklearn.metrics.precision_score
@@ -366,19 +402,8 @@ model_output_file = os.path.join(output_dir, model_output)
 joblib.dump(best_model, model_output_file)
 
 ## Create data frame of predictions
-#predictions_df = pd.DataFrame({'y_pred': y_pred})
-## Save predicitions to CSV file
 model_predictions = args.model_predictions
-#model_predictions_file = os.path.join(output_dir, model_predictions)
-## Do I need to specify 'model_predictions_file' below instead of 'model_predictions'?
-#predictions_df.to_csv(model_predictions, index=False)
-
-
-# Shouldn't need this first command now, since X_test now contains the L2 ground truth data
-
-## Retrieve L2_groundtruth values corresponding to the original indices of the training dataset
-#L2_groundtruth_test_rows = X_test.loc[original_indices_test, 'L2_groundtruth']
-
+# Load CSV with predictions
 print("X_test before reset index:", X_test_CSV)
 ## This assumes that the order of y_pred is the same as the order of X_test
 # Create data frame of predictions
@@ -387,9 +412,6 @@ predictions_df = pd.DataFrame({'y_pred': y_pred})
 X_test_CSV.reset_index(drop=True, inplace=True)
 # Create DF of X_test, y_pred, and corresponding L2_groundtruth values
 combined_df = pd.concat([X_test_CSV, predictions_df], axis=1)
-#combined_df['L2_groundtruth'] = L2_groundtruth_test.values
-## Add the original indices as a separate column in the DataFrame
-#combined_df['original_index'] = shuffled_df.index[X_test.index]
 
 ## To check, 
 print("CHECK INDICES MATCH")
@@ -397,18 +419,10 @@ print("X test (satellite data) with L2 ground truth data:", X_test_CSV)
 print("Model predictions:", predictions_df)
 print("Output CSV data:", combined_df)
 
-## Drop the 'original_index' column
-#combined_df.drop('original_index', axis=1, inplace=True)
-## Save the combined DataFrame to a CSV file
+## Save DF with predictions, satellite data and L2 ground truth to CSV file
+# For use in downstream hierarchical model
 model_predictions_file = os.path.join(output_dir, model_predictions)
-# Export the combined DataFrame to a CSV file
+# Export to CSV file
 combined_df.to_csv(model_predictions_file, index=False)
-
-## Create a DataFrame of predictions and satellite data
-#predictions_df = pd.DataFrame({'y_pred': y_pred})
-#combined_df = pd.concat([X_test, predictions_df], axis=1)
-## Save the combined DataFrame to a CSV file
-#model_predictions_file = os.path.join(output_dir, model_predictions)
-#combined_df.to_csv(model_predictions_file, index=False)
 
 wandb.finish()
